1e66871b034acb0327df9aac5251d058c83ddbbd
Salvatore Mercuri <47568553+smmercuri@users.noreply.github.com>
2025-11-06 11:51:26 +0000

    add the generalized Riemann hypothesis (#1176)
    fixes #448

--- FormalConjectures/Util/ForMathlib.lean
+++ FormalConjectures/Util/ForMathlib.lean
@@ -54,6 +54,7 @@ import FormalConjectures.ForMathlib.Geometry.Euclidean
 import FormalConjectures.ForMathlib.LinearAlgebra.SpecialLinearGroup
 import FormalConjectures.ForMathlib.Logic.Equiv.Fin.Rotate
 import FormalConjectures.ForMathlib.NumberTheory.AdditivelyComplete
+import FormalConjectures.ForMathlib.NumberTheory.DirichletCharacter.Basic
 import FormalConjectures.ForMathlib.NumberTheory.CoveringSystem
 import FormalConjectures.ForMathlib.NumberTheory.Lacunary
 import FormalConjectures.ForMathlib.NumberTheory.PrimeGap
