1bdb946df4132453d7fd0b22a0be8fa77dac2d0d
Salvatore Mercuri <47568553+smmercuri@users.noreply.github.com>
2025-11-23 09:25:01 +0000

    fix: positivity in `erdos_20` (#1261)
    - If `n = 0` then `erdos_20` is false since `f 0 1 = 1 ≥ 1 = c k ^ 0`
    - Also turn `erdos_20` into a question

--- FormalConjectures/ErdosProblems/20.lean
+++ FormalConjectures/ErdosProblems/20.lean
@@ -21,9 +21,6 @@ import FormalConjectures.Util.ProblemImports
 
 *Reference:* [erdosproblems.com/20](https://www.erdosproblems.com/20)
 -/
-
-namespace Erdos20
-
 universe u
 variable {α : Type}
 
@@ -34,6 +31,14 @@ share the same intersection $S$.
 def IsSunflowerWithKernel (F : Set (Set α)) (S : Set α) : Prop :=
   F.Pairwise (fun A B => A ∩ B = S)
 
+@[category test, AMS 5]
+theorem isSunflowerWithKernel_empty (S : Set α) : IsSunflowerWithKernel {} S := by
+  simp [IsSunflowerWithKernel]
+
+@[category test, AMS 5]
+theorem isSunflowerWithKernel_singleton (S : Set α) (A : Set α) :
+    IsSunflowerWithKernel {A} S := by
+  simp [IsSunflowerWithKernel]
 
 /--
 A sunflower $F$ is a collection of sets in which all possible distinct pairs of sets share the
@@ -41,23 +46,36 @@ same intersection.
 -/
 def IsSunflower (F : Set (Set α)) : Prop := ∃ S, IsSunflowerWithKernel F S
 
+@[category test, AMS 5]
+theorem isSunflower_empty : IsSunflower (∅ : Set (Set α)) := by
+  simp [IsSunflower, isSunflowerWithKernel_empty]
+
+@[category test, AMS 5]
+theorem isSunflower_singleton (A : Set α) : IsSunflower {A} := by
+  simp [IsSunflower, isSunflowerWithKernel_singleton]
+
 /--
 Let $f(n,k)$ be minimal such that every $F$ family of $n$-uniform sets with $|F| \ge f(n,k)$
 contains a $k$-sunflower.
 -/
 noncomputable def f (n k : ℕ) : ℕ :=
-    sInf {m | ∀ {α : Type}, ∀ (F : Set (Set α)),
+  sInf {m | ∀ {α : Type}, ∀ (F : Set (Set α)),
     ((∀ f ∈ F, f.ncard = n) ∧ m ≤ F.ncard) → ∃ S ⊆ F, S.ncard = k ∧ IsSunflower S}
 
+@[category test, AMS 5]
+theorem f_0_1 : f 0 1 = 1 := by
+  refine IsLeast.csInf_eq ⟨fun F hF ↦ ?_, fun n hn ↦ n.pos_of_ne_zero fun hn₀ ↦ ?_⟩
+  · obtain ⟨A, hA⟩ := F.nonempty_of_ncard_ne_zero (by omega)
+    exact ⟨{A}, by simpa using ⟨hA, isSunflower_singleton _⟩⟩
+  · obtain ⟨S, hS⟩ := (hn (α := ℕ) {} (by simpa))
+    simp_all [bot_unique hS.1]
 
 /--
-Is it true that $f(n,k) < c^n_k$ for some constant $c_k>0?
+Is it true that $f(n,k) < c_k^n$ for some constant $c_k>0$ and for all $n > 0$?
 -/
 @[category research open, AMS 5]
-theorem erdos_20 : ∃ (c : ℕ → ℕ), ∀ n k, f n k < (c k)^n := by
+theorem erdos_20 :
+    (∃ (c : ℕ → ℕ), ∀ n k, n > 0 → f n k < (c k) ^ n ↔ answer(sorry)) := by
   sorry
 
-
 -- TODO(firsching): add the various known bounds as variants.
-
-end Erdos20
