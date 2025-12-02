c1249b6a157bb39e5120cfe09ef084ae643d4d74
HerrLaal <77057945+HerrLaal@users.noreply.github.com>
2025-12-02 21:06:58 +0100

    add erdős problem 677 (#1235)
    This adds a formalization of Erdős problem 677. If you'd like me to
    change something or to add for example the Thue-Siegel theorem, I'm
    happy to do so :)
    
    fixes #872

--- FormalConjectures/Util/ForMathlib.lean
+++ FormalConjectures/Util/ForMathlib.lean
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -/
 
+import FormalConjectures.ForMathlib.Algebra.GCDMonoid.Finset
 import FormalConjectures.ForMathlib.Algebra.Group.Indicator
 import FormalConjectures.ForMathlib.Algebra.Order.Group.Pointwise.Interval
 import FormalConjectures.ForMathlib.Algebra.Polynomial.Basic
