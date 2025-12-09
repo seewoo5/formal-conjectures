e47eb4e1d83b50b828e6bc32a92673b4667739da
Yaël Dillies <yael.dillies@gmail.com>
2025-12-09 19:40:51 +0100

    chore(ErdosProblems): golf 508 (#1369)

--- FormalConjectures/ErdosProblems/508.lean
+++ FormalConjectures/ErdosProblems/508.lean
@@ -30,25 +30,18 @@ or the [Golomb graph]
 *At least 5 colors are required:* [de Grey 2018](https://arxiv.org/abs/1804.02385)
 -/
 
-
+open SimpleGraph
+open scoped EuclideanGeometry
 
 namespace Erdos508
-
-open scoped EuclideanGeometry
 
 /--
 The unit-distance graph in the plane, i.e. the graph whose vertices are points in the plane
 and whose edges connect points that are exactly 1 unit apart.
 -/
-def UnitDistancePlaneGraph : SimpleGraph ℝ² :=
-  SimpleGraph.mk
-    (fun x y => dist x y = 1)
-    (by
-      intros x y
-      simp [dist_comm])
-    (by
-      intros x
-      simp [dist_self])
+def UnitDistancePlaneGraph : SimpleGraph ℝ² where
+  Adj x y := dist x y = 1
+  symm _ _ := by simp [_root_.dist_comm]
 
 scoped notation "χ(ℝ²)" => UnitDistancePlaneGraph.chromaticNumber
 
@@ -97,47 +90,12 @@ theorem HadwigerNelsonAtMostSeven :
     χ(ℝ²) ≤ 7 := by
   sorry
 
+/-- The chromatic number of the plane is at least 3.
 
-
-/--
-The "chromatic number of the `Plane`" is at least 3. This is proven
-by considering an equilateral triangle in the plane.
--/
+This is proven by considering an equilateral triangle in the plane. -/
 @[category high_school, AMS 5]
-theorem HadwigerNelsonAtLeastThree : 3 ≤ χ(ℝ²) := by
-  unfold SimpleGraph.chromaticNumber
-  apply le_sInf
-  rintro b ⟨n, rfl⟩
-  apply le_iInf
-  norm_cast
-  intro plane_is_n_colorable
-  rw [Set.mem_setOf_eq] at plane_is_n_colorable
-  obtain ⟨n_coloring⟩ := plane_is_n_colorable
-
-  -- Define the points p₁, p₂ and p₃ of an arbitrary equilateral triangle
-  let p₁ : ℝ² := ![0, 0]
-  let p₂ : ℝ² := ![1, 0]
-  let p₃ : ℝ² := ![0.5, Real.sqrt 3 / 2]
-
-  -- Prove that the points are adjacent in the unit distance plane graph
-  have p₁_p₂_adj : UnitDistancePlaneGraph.Adj p₁ p₂ := by simp [UnitDistancePlaneGraph, p₁, p₂, dist]
-  have p₁_p₃_adj : UnitDistancePlaneGraph.Adj p₁ p₃ := by simp [UnitDistancePlaneGraph, p₁, p₃,  dist, div_pow]; norm_num
-  have p₂_p₃_adj : UnitDistancePlaneGraph.Adj p₂ p₃ := by simp [UnitDistancePlaneGraph, p₂, p₃, dist, div_pow]; norm_num
-
-
-  -- We'll denote coloring p_i with c_i (for i ∈ {1,2,3})
-  -- Prove that p₁, p₂ and p₃ have different images under the coloring
-  have not_c₁_equ_c₂ : n_coloring p₁ ≠ n_coloring p₂ := n_coloring.valid p₁_p₂_adj
-  have not_c₁_equ_c₃ : n_coloring p₁ ≠ n_coloring p₃ := n_coloring.valid p₁_p₃_adj
-  have not_c₂_equ_c₃ : n_coloring p₂ ≠ n_coloring p₃ := n_coloring.valid p₂_p₃_adj
-
-  let used_colors_subset : Finset (Fin n) := {n_coloring p₁, n_coloring p₂, n_coloring p₃}
-  have card_eq_3 : used_colors_subset.card = 3 := (used_colors_subset.card_eq_three.2
-    ⟨n_coloring p₁, n_coloring p₂, n_coloring p₃,
-      ⟨not_c₁_equ_c₂, not_c₁_equ_c₃, not_c₂_equ_c₃, rfl⟩
-    ⟩)
-
-  calc
-    3 = used_colors_subset.card := card_eq_3.symm
-    _ ≤ Fintype.card (Fin n) := Finset.card_le_univ used_colors_subset
-    _ = n := Fintype.card_fin n
+theorem HadwigerNelsonAtLeastThree : 3 ≤ χ(ℝ²) :=
+  le_chromaticNumber_of_pairwise_adj (by simp) ![!₂[0, 0], !₂[1, 0], !₂[0.5, Real.sqrt 3 / 2]] <| by
+    simp [pairwise_fin_succ_iff_of_isSymm, Fin.forall_fin_succ]
+    simp [UnitDistancePlaneGraph, PiLp.dist_eq_of_L2, Real.dist_eq, div_pow]
+    norm_num
