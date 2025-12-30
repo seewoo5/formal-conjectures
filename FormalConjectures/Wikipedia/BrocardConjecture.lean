7fbd9a75f590199bcc059ce91278b45cf166621c
Seewoo Lee <seewoo5@gmail.com>
2025-12-31 06:32:35 +0900

    move open

--- FormalConjectures/Wikipedia/BrocardConjecture.lean
+++ FormalConjectures/Wikipedia/BrocardConjecture.lean
@@ -16,8 +16,6 @@ limitations under the License.
 
 import FormalConjectures.Util.ProblemImports
 
-open Filter
-
 /-!
 # Brocard's Conjecture
 
@@ -26,7 +24,7 @@ open Filter
 - [Luan Alberto Ferreira, *Real exponential sums over primes and prime gaps*](https://arxiv.org/abs/2307.08725)
 -/
 
-open Finset
+open Finset Filter
 
 namespace Brocard
 
