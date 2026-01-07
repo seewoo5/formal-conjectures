9c2f5e6d8f57a76f6fd9785c4a8d3f8c74e7a2c9
Yaël Dillies <yael.dillies@gmail.com>
2026-01-07 09:01:09 +0100

    chore(ErdosProblems/477): mark as disproved (#1510)
    and update to the new statement from https://www.erdosproblems.com/477
    
    ---------
    
    Co-authored-by: Moritz Firsching <firsching@google.com>

--- FormalConjectures/ErdosProblems/477.lean
+++ FormalConjectures/ErdosProblems/477.lean
@@ -19,27 +19,65 @@ import FormalConjectures.Util.ProblemImports
 /-!
 # Erdős Problem 477
 
-*Reference:* [erdosproblems.com/477](https://www.erdosproblems.com/477)
+*References:*
+- [erdosproblems.com/477](https://www.erdosproblems.com/477)
+- [Sek59](http://dml.cz/dmlcz/100376) Milan Sekanina, Замечания к фактoризации беcкoнечнoй цикличеcкoй группы, Czechoslovak Mathematical Journal, Vol. 9 (1959), No. 4, 485–495
 -/
+
+open Polynomial Set
 
 namespace Erdos477
 
 /--
-Let $f:\mathbb{Z}\to \mathbb{Z}$ be a polynomial of degree at least $2$. Is there a set $A$ such
-that every $z\in \mathbb{Z}$ has exactly one representation as $z=a+f(n)$ for some $a\in A$ and
-$n > 0$?
+Is there a polynomial $f:\mathbb{Z}\to \mathbb{Z}$ of degree at least $2$ and a set
+$A\subset \mathbb{Z}$ such that for any $z\in \mathbb{Z}$ there is exactly one $a\in A$ and
+$b\in \{ f(n) : n\in\mathbb{Z}\}$ such that $z=a+b$?
 -/
 @[category research open, AMS 12]
-theorem erdos_477 : answer(sorry) ↔ ∀ (f : Polynomial ℤ), 2 ≤ f.degree →
-    (∃ (A : Set ℤ), ∀ z, ∃! a ∈ A ×ˢ (f.eval '' {n | 0 < n}), z = a.1 + a.2) := by
+theorem erdos_477 : answer(sorry) ↔
+    ∃ f : ℤ[X], 2 ≤ f.degree ∧ ∃ A : Set ℤ,
+      ∀ z, ∃! ab ∈ A ×ˢ (f.eval '' {n | 0 < n}), z = ab.1 + ab.2 := by
   sorry
 
 /--
-Probably there is no such $A$ for any polynomial $f$.
+There is no such $A$ for the polynomial $f(x) = X^2$.
+
+This is shown in [Sek59].
+-/
+@[category research solved, AMS 12]
+theorem erdos_477.S_sq :
+    letI f := X ^ 2
+    ∀ A : Set ℤ, ∃ z, ¬ ∃! a ∈ A ×ˢ (f.eval '' {n | 0 < n}), z = a.1 + a.2 := by
+  sorry
+
+/--
+There is no such $A$ for any polynomial $f(x) = aX^2 + bX + c$, if $a | b$
+with $a \ne 0$ and $b \ne 0.
+This was found be AlphaProof for the specific instance $X^2 - X + 1$ and then generalised.
+ -/
+@[category research solved, AMS 12]
+theorem erdos_477.degree_two_dvd_condition_b_ne_zero {a b c : ℤ} (ha : a ≠ 0) (hb : b ≠ 0)
+    (hab : a ∣ b) :
+    let f := a • X ^ 2 + b • X + C c
+    ∀ A : Set ℤ, ∃ z, ¬ ∃! a ∈ A ×ˢ (f.eval '' {n | 0 < n}), z = a.1 + a.2 := by
+  sorry
+
+/--
+Probably there is no such $A$ for the polynomial $X^3$.
 -/
 @[category research open, AMS 12]
-theorem erdos_477.variants.strong_negation (f : Polynomial ℤ) (hf₀ : 2 ≤ f.degree) : ¬ ∃ (A : Set ℤ),
-    ∀ z, ∃! a ∈ A ×ˢ (f.eval '' {n | 0 < n}), z = a.1 + a.2 := by
+theorem erdos_477.X_pow_three :
+    letI f := X ^ 3
+    ∀ A : Set ℤ, ∃ z, ¬ ∃! a ∈ A ×ˢ (f.eval '' {n | 0 < n}), z = a.1 + a.2 := by
+  sorry
+
+/--
+Probably there is no such $A$ for the polynomial $X^k$ for any $k \ge 2$. This is asked in [Sek59].
+-/
+@[category research open, AMS 12]
+theorem erdos_477.monomial (k : ℕ) (hk : 2 ≤ k) :
+    letI f := X ^ k
+    ∀ A : Set ℤ, ∃ z, ¬ ∃! a ∈ A ×ˢ (f.eval '' {n | 0 < n}), z = a.1 + a.2 := by
   sorry
 
 end Erdos477
