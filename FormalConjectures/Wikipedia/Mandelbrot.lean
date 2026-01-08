f79a2d777672fb5cb9934f9b87a4d3f12fe1d729
Moritz Firsching <firsching@google.com>
2026-01-08 12:45:24 +0100

    fix `density_of_hyperbolicity_general_exponent` (#1569)

--- FormalConjectures/Wikipedia/Mandelbrot.lean
+++ FormalConjectures/Wikipedia/Mandelbrot.lean
@@ -117,12 +117,12 @@ theorem MLC_general_exponent (n : ℕ) : LocallyConnectedSpace (multibrotSet n) 
 `n`-periodic point (i.e. `f^[n] z = z`), `f^[n]` is differentiable at `z` and `‖deriv f^[n] z‖` is
 strictly less than one.-/
 def IsAttractingCycle (f : ℂ → ℂ) (n : ℕ) (z : ℂ) : Prop :=
-  f.IsPeriodicPt n z ∧ DifferentiableAt ℂ f^[n] z ∧ ‖deriv f^[n] z‖ < 1
+  (0 < n) ∧ f.IsPeriodicPt n z ∧ DifferentiableAt ℂ f^[n] z ∧ ‖deriv f^[n] z‖ < 1
 
 /-- For example, `0` is part of an attracting `2`-cycle of `z ↦ z ^ 2 - 1`. -/
 @[category test, AMS 37]
 theorem isAttractingCycle_z_squared_minus_one : IsAttractingCycle (fun z ↦ z ^ 2 - 1) 2 0 :=
-  ⟨by simp [IsPeriodicPt, IsFixedPt], by fun_prop, by simp [deriv_comp]⟩
+  ⟨by decide, by simp [IsPeriodicPt, IsFixedPt], by fun_prop, by simp [deriv_comp]⟩
 
 /-- On the other hand, while `2` is part of a `1`-cycle of `z ↦ z ^ 2 - 2`, that cycle is not
 attracting. -/
@@ -140,7 +140,7 @@ theorem no_attractingCycle_period_zero (f : ℂ → ℂ) (z : ℂ) : ¬ IsAttrac
 `fun z ↦ z ^ 2 + c` has an attracting cycle is dense in the Mandelbrot set. -/
 @[category research open, AMS 37]
 theorem density_of_hyperbolicity :
-    mandelbrotSet ⊆ closure {c | ∃ n z, IsAttractingCycle (fun z ↦ z ^ 2 + c) n z} := by
+    mandelbrotSet ⊆ closure {c | ∃ m z, IsAttractingCycle (fun z ↦ z ^ 2 + c) m z} := by
   sorry
 
 /-- The density of hyperbolicity conjecture for Multibrot sets, stating that the set of all
@@ -148,7 +148,7 @@ parameters `c` for which `fun z ↦ z ^ n + c` has an attracting cycle is dense 
 Note that we need to require `2 ≤ n` because the conjecture is trivially false for `n = 1`. -/
 @[category research open, AMS 37]
 theorem density_of_hyperbolicity_general_exponent {n : ℕ} (hn : 2 ≤ n) :
-    multibrotSet n ⊆ closure {c | ∃ n z, IsAttractingCycle (fun z ↦ z ^ n + c) n z} := by
+    multibrotSet n ⊆ closure {c | ∃ m z, IsAttractingCycle (fun z ↦ z ^ n + c) m z} := by
   sorry
 
 /-- The boundary of any Multibrot set is measurable because it is closed, so it makes sense to
