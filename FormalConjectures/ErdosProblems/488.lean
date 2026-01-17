19806e80f9f0eac254c2a59317d1d3cea90cc579
Moritz Firsching <firsching@google.com>
2026-01-17 21:22:24 +0100

    feat(ErdosProblems/488): update (#1647)
    fixes #1534
    
    ---------
    
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- FormalConjectures/ErdosProblems/488.lean
+++ FormalConjectures/ErdosProblems/488.lean
@@ -28,13 +28,13 @@ namespace Erdos488
 
 /--
 Let $A$ be a finite set and
-$$B = \{n \ge 1 : a \nmid n \text{ for all } a \in A\}.$$
-Is it true that, for every $m > n \ge \max(A)$,
-$$\frac{|B \cap [1, m]|}{m} < 2 \frac{|B \cap [1, n]|}{n}?$$
+$$B=\{ n \geq 1 : a\mid n\textrm{ for some }a\in A\}.$$
+Is it true that, for every $m>n\geq \max(A)$,
+$$\frac{\lvert B\cap [1,m]\rvert }{m}< 2\frac{\lvert B\cap [1,n]\rvert}{n}?$$
 -/
-@[category research solved, AMS 5 11]
-theorem erdos_488 : answer(False) ↔ ∀ (A : Finset ℕ), A.Nonempty → 0 ∉ A → 1 ∉ A →
-    letI B := {n ≥ 1 | ∀ a ∈ A, ¬ a ∣ n}
+@[category research open, AMS 5 11]
+theorem erdos_488 : answer(sorry) ↔ ∀ (A : Finset ℕ), A.Nonempty →
+    letI B := {n ≥ 1 | ∃ a ∈ A, a ∣ n}
     ∀ᵉ (n : ℕ) (m > n), A.max ≤ n →
       ((Finset.Icc 1 m).filter (· ∈ B)).card / (m : ℚ) <
         2 * ((Finset.Icc 1 n).filter (· ∈ B)).card / n := by
