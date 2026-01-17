ef123bf99598fb318473ab272ba1eb7b176663c2
Seewoo Lee <seewoo5@gmail.com>
2026-01-17 12:38:15 -0800

    add as Green's problem 81

---
+++ FormalConjectures/GreensOpenProblems/81.lean
@@ -0,0 +1,27 @@
+/-
+Copyright 2025 The Formal Conjectures Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-/
+import FormalConjectures.Util.ProblemImports
+import FormalConjectures.ErdosProblems.«510»
+
+/-!
+# Ben Green's Open Problem 81
+
+Let $A$ be a set of size $n$ integers. Is there some absolute constant $c > 0$ and $\theta$ such that $\sum_{a \in A} \cos(a \theta) \leq - c \sqrt{n}$?
+
+*Reference:*
+ - [Ben Green's Open Problem 81](https://people.maths.ox.ac.uk/greenbj/papers/open-problems.pdf#section.11 Problem 81)
+ - [erdosproblems.com/510](https://www.erdosproblems.com/510)
+-/
