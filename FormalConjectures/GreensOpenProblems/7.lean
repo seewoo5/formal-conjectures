cabf4520cf89aae6733549b096db2f82246b3867
Daniel Chin <danielchin93@gmail.com>
2026-01-22 01:07:03 -0800

    feat(ErdosProblems): 341 (#1778)
    Closes #500 
    
    Formalizes Erdos Problem 341: https://www.erdosproblems.com/341

---
+++ FormalConjectures/GreensOpenProblems/7.lean
@@ -0,0 +1,34 @@
+/-
+Copyright 2026 The Formal Conjectures Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-/
+
+import FormalConjectures.Util.ProblemImports
+import FormalConjectures.ErdosProblems.«341»
+
+/-!
+# Ben Green's Open Problem 7
+
+*Reference:* [Ben Green's Open Problem 7](https://people.maths.ox.ac.uk/greenbj/papers/open-problems.pdf#section.1 Problem 7)
+-/
+
+namespace Green7
+
+-- TODO: Add Green's Open Problem 7
+
+@[category research open, AMS 11]
+theorem green_7.variant.queneau : type_of% Erdos341.erdos_341 := by
+  sorry
+
+end Green7
