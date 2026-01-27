169aaebeb73230c97bcf87e2a3b73ed2b202de46
Yaël Dillies <yael.dillies@gmail.com>
2026-01-27 07:56:12 +0100

    fix(ErdosProblems/705): `∧` should be `→` (#1900)

--- FormalConjectures/ErdosProblems/705.lean
+++ FormalConjectures/ErdosProblems/705.lean
@@ -34,7 +34,7 @@ Is there some k such that if G has girth ≥ k, then χ(G) ≤ 3?
 @[category research open, AMS 5]
 theorem erdos_705:
   answer(sorry) ↔ ∃ k, ∀ V : Set ℝ², V.Finite →
-    (UnitDistancePlaneGraph V).girth ≥ k ∧ (UnitDistancePlaneGraph V).chromaticNumber ≤ 3 := by
+    (UnitDistancePlaneGraph V).girth ≥ k → (UnitDistancePlaneGraph V).chromaticNumber ≤ 3 := by
   sorry
 
 
