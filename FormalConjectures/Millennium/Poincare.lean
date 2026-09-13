94b2a7f9ff17b56e4f607e91744d1b123d449062
Felix Pernegger <s59fpern@uni-bonn.de>
2026-09-13 10:27:07 +0000

    chore: fix universe issue in poincare conjecture (#5951)
    I dont think it makes sense to restrict to `Type`. This is equivalent
    however, since connected compact manifolds cant have cardinality > c
    anyways (thus one can lift the manifolds between universes). So this is
    just a nicer version IMO.

--- FormalConjectures/Millennium/Poincare.lean
+++ FormalConjectures/Millennium/Poincare.lean
@@ -27,6 +27,8 @@ References:
 The formalisations in this file are based on the ones written by Junyan Xu in Mathlib4.
 -/
 
+universe u
+
 namespace PoincareConjecture
 
 open scoped Manifold ContDiff EuclideanGeometry ContinuousMap
@@ -74,7 +76,7 @@ to the sphere. As in `ConjectureFor`, the manifold must be Hausdorff: `ChartedSp
 `IsManifold` do not imply this, and in every positive dimension there is a non-Hausdorff smooth
 manifold that is homotopy equivalent to the sphere. -/
 def SmoothConjectureFor (n : ℕ) : Prop :=
-  ∀ (M : Type) [TopologicalSpace M] [T2Space M] [ChartedSpace (ℝ^n) M]
+  ∀ (M : Type u) [TopologicalSpace M] [T2Space M] [ChartedSpace (ℝ^n) M]
     [IsManifold (𝓡 n) ∞ M], M ≃ₕ 𝕊ⁿ → Nonempty (M ≃ₘ⟮𝓡 n, 𝓡 n⟯ 𝕊ⁿ)
 
 /-- A reformulation of the Millennium Problem in terms of smooth 3-folds. -/
